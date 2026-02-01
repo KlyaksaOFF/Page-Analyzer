9cd4773a700ef99d107e78bad8ebb96b932bac7d
Alex <klyaksaoff@gmail.com>
2026-02-01 14:50:52 +0300

    update

--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -11,8 +11,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 def get_db_connection():
     if not DATABASE_URL:
         raise ValueError("DATABASE_URL not set in environment variables")
-
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    conn = psycopg2.connect(DATABASE_URL)
     conn.autocommit = True
     return conn
 
